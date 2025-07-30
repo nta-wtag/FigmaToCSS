5b1a543b4acb7c2cfcbf9bb502f35b7d02f09d56
nta-wtag <noushin.tasnim@welldev.io>
2025-07-30 14:05:05 +0600

    Review changes

--- script.js
+++ script.js
@@ -1,87 +1,73 @@
 const dataTableBody = document.querySelector('#dataTable tbody');
-const totalCustomercount = document.getElementById('totalCustomerCount')
-const activeMemberCount = document.getElementById('totalActiveMembers')
-const filter = document.getElementById('filter')
+const totalCustomerCount = document.getElementById('totalCustomerCount')
+const totalActiveMemberCount = document.getElementById('totalActiveMembers')
+const sort = document.getElementById('sort')
 
-const localJsonData = [
-            { "customerName": "Jane Cooper", "company": "Microsoft", "phoneNumber": "(225) 555-0118", "email": "jane@microsoft.com", "country": "United States", "status": "Active" },
-            { "customerName": "Floyd Miles", "company": "Yahoo", "phoneNumber": "(205) 555-0100", "email": "floyd@yahoo.com", "country": "Timor-Leste", "status": "Inactive" },
-            { "customerName": "Ronald Richards", "company": "Adobe", "phoneNumber": "(302) 555-0107", "email": "ronald@adobe.com", "country": "Paraguay", "status": "Active" },
-            { "customerName": "Marvin McKinney", "company": "Tesla", "phoneNumber": "(252) 555-0126", "email": "marvin@tesla.com", "country": "Ireland", "status": "Inactive" },
-            { "customerName": "Jerome Bell", "company": "Google", "phoneNumber": "(629) 555-0129", "email": "jerome@google.com", "country": "Vietnam", "status": "Active" },
-            { "customerName": "Kathryn Murphy", "company": "Microsoft", "phoneNumber": "(406) 555-0120", "email": "kathryn@microsoft.com", "country": "Mexico", "status": "Active" },
-            { "customerName": "Jacob Jones", "company": "Yahoo", "phoneNumber": "(208) 555-0112", "email": "jacob@yahoo.com", "country": "Brazil", "status": "Active" }
-        ];
+const ACTIVE = 'Active'
 
-        function addTableRowToDOM(data) {
-            const row = dataTableBody.insertRow();
+const customerData = [
+    { "name": "Jane Cooper", "company": "Microsoft", "phoneNumber": "(225) 555-0118", "email": "jane@microsoft.com", "country": "United States", "status": "Active" },
+    { "name": "Floyd Miles", "company": "Yahoo", "phoneNumber": "(205) 555-0100", "email": "floyd@yahoo.com", "country": "Timor-Leste", "status": "Inactive" },
+    { "name": "Ronald Richards", "company": "Adobe", "phoneNumber": "(302) 555-0107", "email": "ronald@adobe.com", "country": "Paraguay", "status": "Active" },
+    { "name": "Marvin McKinney", "company": "Tesla", "phoneNumber": "(252) 555-0126", "email": "marvin@tesla.com", "country": "Ireland", "status": "Inactive" },
+    { "name": "Jerome Bell", "company": "Google", "phoneNumber": "(629) 555-0129", "email": "jerome@google.com", "country": "Vietnam", "status": "Active" },
+    { "name": "Kathryn Murphy", "company": "Microsoft", "phoneNumber": "(406) 555-0120", "email": "kathryn@microsoft.com", "country": "Mexico", "status": "Active" },
+    { "name": "Jacob Jones", "company": "Yahoo", "phoneNumber": "(208) 555-0112", "email": "jacob@yahoo.com", "country": "Brazil", "status": "Active" }
+];
 
-            const nameCell = row.insertCell();
-            nameCell.textContent = data.customerName || '';
+function addTableRowToDOM(data) {
+    const row = dataTableBody.insertRow();
 
-            const companyCell = row.insertCell();
-            companyCell.textContent = data.company || '';
+    const nameCell = row.insertCell();
+    nameCell.textContent = data.name || '';
 
-            const phoneCell = row.insertCell();
-            phoneCell.textContent = data.phoneNumber || '';
+    const companyCell = row.insertCell();
+    companyCell.textContent = data.company || '';
 
-            const emailCell = row.insertCell();
-            emailCell.textContent = data.email || '';
+    const phoneCell = row.insertCell();
+    phoneCell.textContent = data.phoneNumber || '';
 
-            const countryCell = row.insertCell();
-            countryCell.textContent = data.country || '';
+    const emailCell = row.insertCell();
+    emailCell.textContent = data.email || '';
 
-            const statusCell = row.insertCell();
-            statusCell.textContent = data.status || '';
-            statusCell.classList.add(data.status ? data.status.toLowerCase() : '');
-        }
+    const countryCell = row.insertCell();
+    countryCell.textContent = data.country || '';
 
-        function updateCustomerCount(data){
-            totalCustomercount.textContent = data.length
-            activeMemberCount.textContent = data.filter(customer=> customer.status==='Active').length
-        }
+    const statusCell = row.insertCell();
+    statusCell.textContent = data.status || '';
+    statusCell.classList.add(data.status ? data.status.toLowerCase() : '');
+}
 
-        function sortTable() {
-            dataTableBody.innerHTML = ''
-            const selectedStatus = filter.value;
+function updateCustomerCount(data){
+    totalCustomerCount.textContent = data.length
+    totalActiveMemberCount.textContent = data.filter(customer=> customer.status===ACTIVE).length
+}
 
-            let filteredData = [...localJsonData]
-            console.log(selectedStatus)
-            if (selectedStatus =='country') {
-                filteredData.sort((a, b) => {
-                    const countryA = a.country.toLowerCase();
-                    const countryB = b.country.toLowerCase();
+function sorting(selectedOption){
+    return function(a,b){
+        const fieldA = a[selectedOption.toLowerCase()].toLowerCase();
+        const fieldB = b[selectedOption.toLowerCase()].toLowerCase();
+    
+        return fieldA.localeCompare(fieldB)
+    }
+}
 
-                    return countryA.localeCompare(countryB);
-                });
-            }
-            else if(selectedStatus==='company'){
-                filteredData.sort((a, b) => {
-                    const countryA = a.company.toLowerCase();
-                    const countryB = b.company.toLowerCase();
+function sortTable() {
+    dataTableBody.innerHTML = ''
+    const selectedOption = sort.value;
 
-                    return countryA.localeCompare(countryB);
-                });
-            }
-            else if(selectedStatus==='name'){
-                filteredData.sort((a, b) => {
-                    const countryA = a.customerName.toLowerCase();
-                    const countryB = b.customerName.toLowerCase();
+    customerData.sort(sorting(selectedOption));
 
-                    return countryA.localeCompare(countryB);
-                });
-            }
+    if (customerData.length > 0) {
+        customerData.forEach(customer => addTableRowToDOM(customer));
+    } else {
+        alert("No data matches the sort criteria.");
+    }
+}
 
-            if (filteredData.length > 0) {
-                filteredData.forEach(customer => addTableRowToDOM(customer));
-            } else {
-                console.log("No data matches the filter criteria.");
-            }
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    sortTable()
+    updateCustomerCount(customerData)
+});
 
-        document.addEventListener('DOMContentLoaded', () => {
-            sortTable()
-            updateCustomerCount(localJsonData)
-        });
-
-        filter.addEventListener('change', sortTable)
+filter.addEventListener('change', sortTable)
